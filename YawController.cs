846b7b59b45afe00ae8a44716b7a39352a0f981f
Joseph DeMartini <josephdemartini801@gmail.com>
2025-04-28 10:01:17 -0400

    dwa

--- YawController.cs
+++ YawController.cs
@@ -45,7 +45,7 @@ namespace YawSafety
             Console.WriteLine("Connected.");
 
             Socket socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
-            socket.Bind(new IPEndPoint(IPAddress.Broadcast, 25565));    
+            socket.Bind(new IPEndPoint(IPAddress.Parse("255.255.255.255"), 25565));    
             while(Activated)
             {
                 Console.WriteLine("Transmitted");
